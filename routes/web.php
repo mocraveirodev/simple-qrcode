cc19dda5fd48fdd670d8e8b2b48369f89e924c42
Monica Craveiro <craveiromonica@gmail.com>
2024-04-20 02:26:29 -0300

    redirect home endpoint to qrcode

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\QRCodeController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('qrcode');
 });
 
 Route::get('/qr-code', [QRCodeController::class, 'index'])
