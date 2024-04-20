ba08dfd4003d7840eda891cd4a9e543fcdfd2b9c
Monica Craveiro <craveiromonica@gmail.com>
2024-04-20 02:36:45 -0300

    remove home route

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\QRCodeController;
 
-
-Route::get('/', function () {
-    return redirect()->route('qrcode');
-});
-
 Route::get('/qr-code', [QRCodeController::class, 'index'])
     ->name('qrcode');
